1437ea47e7d8543659ab0468d5a469b418279abb
NikolayNesterchuk <nn@handiscover.com>
2024-05-28 14:38:55 +0200

    Updated text in InfoCard

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -10,10 +10,20 @@ const InfoCard = () => {
             <CardContent>
                 <Typography variant={'h5'}>Other infos</Typography>
                 <Box mt={2}>
-                    <ul>
+                    <ul style={{display: 'flex', flexDirection: 'column', gap: 10}}>
                         <li>
                             <Typography>
                                 You can change the application theme by going to <code>src/theme.ts</code> and changing <code>palette.mode</code> from <code>'dark'</code> to <code>'light'</code>
+                            </Typography>
+                        </li>
+                        <li>
+                            <Typography>
+                                The project has a <span style={{color: '#e80000', fontWeight: 500}}>crutch</span> with pagination.
+                                API that is used for posts data does not give count of items.
+                                When loading posts for the first time,
+                                I had to query all items to store the <code>count</code>.
+                                In normal cases, the endpoint that gives the list should have a <code>count</code> or <code>hasNextPage</code>.
+                                This is located in file <code>postSlice.ts</code> in thunk <code>fetchPosts</code>
                             </Typography>
                         </li>
                     </ul>
