5f19fbb4201680d6895eb7503a6fd8bddbf48115
NikolayNesterchuk <nn@handiscover.com>
2024-05-29 10:17:56 +0200

    Added description for home page / removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import NextLink from 'next/link';
 import { Metadata } from 'next';
 import CustomBreadcrumbs from "@/components/custom-breadcrumbs/CustomBreadcrumbs";
 import {Grid} from "@mui/material";
@@ -14,7 +11,7 @@ import InfoCard from "@/components/InfoCard";
 
 export const metadata: Metadata = {
     title: 'Blog',
-    description: 'Blog description',
+    description: 'Explore our latest articles and updates. Learn about new features, best practices, and more.',
 };
 
 const App = () => {
